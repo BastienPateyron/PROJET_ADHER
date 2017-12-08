6f260288532959958f4302b41550746376276926
RemiHosatte <remi.hosatte@gmail.com>
2017-12-08 20:45:06 +0100

    Create CustomList for Secteur
    Set app in FULLSCREEN

--- app/src/main/java/team_adher/adher/fragments/SecteurAdapter.java
+++ app/src/main/java/team_adher/adher/fragments/SecteurAdapter.java
@@ -1,6 +1,9 @@
 package team_adher.adher.fragments;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -47,7 +50,7 @@ public class SecteurAdapter extends ArrayAdapter<Secteur>{
             convertView = inflater.inflate(R.layout.secteur_list_item,parent,false);
             viewHolder.secteur_number = (TextView) convertView.findViewById(R.id.secteur_item_number);
             viewHolder.secteur_nom = (TextView) convertView.findViewById(R.id.secteur_item_nom);
-
+            viewHolder.delete_item = (ImageButton) convertView.findViewById(R.id.secteur_item_trash);
             // Cache the viewHolder object inside the fresh view
             convertView.setTag(viewHolder);
         }
@@ -58,6 +61,31 @@ public class SecteurAdapter extends ArrayAdapter<Secteur>{
         // setText avec notre object
         viewHolder.secteur_number.setText(String.valueOf(secteur.getNumero()));
         viewHolder.secteur_nom.setText(secteur.getNom());
+        viewHolder.delete_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(getContext());
+                }
+                builder.setTitle("Delete entry")
+                        .setMessage("Are you sure you want to delete this entry?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // continue with delete
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
 
         return convertView;
     }

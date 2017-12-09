7f9bb6f2e1699114ab731859f9a34de3579dafea
BastienPateyron <bastien.pateyron@gmail.com>
2017-12-09 17:12:38 +0100

    Couleur datepicker

--- app/src/main/java/team_adher/adher/fragments/Intervention_fragment.java
+++ app/src/main/java/team_adher/adher/fragments/Intervention_fragment.java
@@ -1,14 +1,18 @@
 package team_adher.adher.fragments;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -26,8 +30,33 @@ public class Intervention_fragment extends Fragment {
     EditText date_fin_contrat;
     final Calendar myCalendar = Calendar.getInstance();
 
+    /* Debut test pour changer la couleur */
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+        TextView textView;
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+            // Use the current date as the default date in the date picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
 
+            // Create DatePickerDialog new instance
+            DatePickerDialog datePicker = new DatePickerDialog(getActivity(), this, year, month, day);
+
+            // return the DatePickerDialog
+            return datePicker;
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            // Set the date to textview
+            // Month value start with zero, we have to add by one
+            textView.setText(dayOfMonth + "/" + (monthOfYear+1) + "/" + year);
+        }
+    } /* TEST Pour changer la couleur */
 
     @Nullable
     @Override
@@ -37,11 +66,13 @@ public class Intervention_fragment extends Fragment {
         myView = inflater.inflate(R.layout.form_adherent_layout,container,false);
         date_fin_contrat = (EditText) myView.findViewById(R.id.date_fin_contrat);
 
+
+
+
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                // TODO Auto-generated method stub
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
@@ -49,13 +80,14 @@ public class Intervention_fragment extends Fragment {
             }
         };
 
+        final DatePickerDialog datePicker = new DatePickerDialog(getActivity(), R.style.DatePicker, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH));
+
+
         date_fin_contrat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO Auto-generated method stub
-                new DatePickerDialog(getActivity(), date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                datePicker.show();
             }
         });
 

1f4aa96f9c877b0046377b76908fac16e476af1a
locky12 <30555838+locky12@users.noreply.github.com>
2018-01-03 15:40:43 +0100

    Ajout client

---
+++ app/src/main/java/team_adher/adher/fragments/client/Client_fragment_ajout.java
@@ -0,0 +1,119 @@
+package team_adher.adher.fragments.client;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import team_adher.adher.MainActivity;
+import team_adher.adher.R;
+import team_adher.adher.bdd.ClientDAO;
+import team_adher.adher.classes.Client;
+
+/**
+ * Created by watson on 21/12/2017.
+ */
+
+public class Client_fragment_ajout extends Fragment {
+
+    View myView;
+    boolean validate = true;
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.client_layout_add, container, false);
+
+        getActivity().setTitle("Nouveau Client");
+
+        Button button_add_client = (Button) myView.findViewById(R.id.button_add_client);
+
+        button_add_client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText value_nom = (EditText) myView.findViewById(R.id.value_nom);
+                EditText value_prenom = (EditText) myView.findViewById(R.id.value_prenom);
+                EditText value_phone = (EditText) myView.findViewById(R.id.value_phone);
+                EditText value_num_rue = (EditText) myView.findViewById(R.id.value_num_rue1);
+                EditText value_nom_rue = (EditText) myView.findViewById(R.id.value_nom_rue1);
+                EditText value_cp = (EditText) myView.findViewById(R.id.value_cp1);
+                EditText value_ville_client = (EditText) myView.findViewById(R.id.value_ville1);
+
+
+                String nom = value_nom.getText().toString();
+                String prenom = value_prenom.getText().toString();
+                String phone = value_phone.getText().toString();
+                String num_rue = value_num_rue.getText().toString();
+                String nom_rue = value_nom_rue.getText().toString();
+                String codepostal = value_cp.getText().toString();
+                String ville = value_ville_client.getText().toString();
+
+                if ( TextUtils.isEmpty(nom)) {
+                    Toast.makeText(getContext(), "Nom manquant", Toast.LENGTH_SHORT).show();
+                    validate = false;
+                }
+                if (TextUtils.isEmpty(prenom)) {
+                    Toast.makeText(getContext(), "Prènom manquant", Toast.LENGTH_SHORT).show();
+                    validate = false;
+                }
+                if (TextUtils.isEmpty(phone)) {
+                    Toast.makeText(getContext(), "Numero dd téléphone manquant", Toast.LENGTH_SHORT).show();
+                    validate = false;
+                }
+                if (TextUtils.isEmpty(num_rue)) {
+                    Toast.makeText(getContext(), "Numero de rue manquant", Toast.LENGTH_SHORT).show();
+                    validate = false;
+                }
+                if (TextUtils.isEmpty(nom_rue)) {
+                    Toast.makeText(getContext(), "Nom de rue manquant", Toast.LENGTH_SHORT).show();
+                    validate = false;
+                }
+                if (TextUtils.isEmpty(ville)) {
+                    Toast.makeText(getContext(), "Ville manquante", Toast.LENGTH_SHORT).show();
+                    validate = false;
+                }
+                if (TextUtils.isEmpty(codepostal)) {
+                    Toast.makeText(getContext(), "Code postal manquant", Toast.LENGTH_SHORT).show();
+                    validate = false;
+                }
+
+                if (validate)
+                {
+                    System.out.println("Insert");
+                    ((MainActivity) getActivity()).closekeyboard(getContext(), myView);
+                    ClientDAO clientDAO = new ClientDAO(myView.getContext());
+                    Client client = new Client
+                            (
+                                    0,
+                                    nom,
+                                    prenom,
+                                    Integer.parseInt(phone),
+                                    Integer.parseInt(num_rue),
+                                    nom_rue,
+                                    Integer.parseInt(codepostal),
+                                    ville
+                            );
+
+                   clientDAO.insertClient(client);
+
+
+                    ((MainActivity) getActivity()).changeFragment(new Client_fragment());
+                }
+            }
+        });
+
+        return myView;
+    }
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/team_adher/adher/fragments/client/Client_fragment_modif.java
@@ -0,0 +1,103 @@
+package team_adher.adher.fragments.client;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import team_adher.adher.MainActivity;
+import team_adher.adher.R;
+import team_adher.adher.bdd.ClientDAO;
+import team_adher.adher.classes.Client;
+/**
+ * Created by watson on 21/12/2017.
+ */
+
+public class Client_fragment_modif extends Fragment {
+
+    View myView;
+    private int id_client;
+
+    @SuppressLint("SetTextI18n")
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        myView = inflater.inflate(R.layout.client_layout_info, container, false);
+
+        Bundle bundle = this.getArguments();
+
+        if (bundle != null) {
+            id_client = Integer.valueOf(bundle.get("id_client").toString());
+        }
+        System.out.println("Id Client: " + id_client);
+
+        final EditText value_nom = (EditText) myView.findViewById(R.id.value_nom);
+        final EditText value_prenom = (EditText) myView.findViewById(R.id.value_prenom);
+        final EditText value_phone = (EditText) myView.findViewById(R.id.value_phone);
+        final EditText value_num_rue = (EditText) myView.findViewById(R.id.value_num_rue1);
+        final EditText value_nom_rue = (EditText) myView.findViewById(R.id.value_nom_rue1);
+        final EditText value_cp = (EditText) myView.findViewById(R.id.value_cp1);
+        final EditText value_ville = (EditText) myView.findViewById(R.id.value_ville1);
+
+
+
+        final ClientDAO clientDAO = new ClientDAO(getContext());
+        final Client client = clientDAO.retrieveClient(id_client);
+
+        value_nom.setText(client.getNom());
+        value_prenom.setText(client.getPrenom());
+        value_phone.setText("" + client.getPhone());
+        value_num_rue.setText("" + client.getNum_rue());
+        value_nom_rue.setText(client.getNom_rue());
+        value_cp.setText("" + client.getCp());
+        value_ville.setText(client.getVille());
+
+
+
+        Button button_remove_client = (Button) myView.findViewById(R.id.remove_client);
+
+        button_remove_client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clientDAO.deleteClient(getContext(), client.getId());
+                ((MainActivity) getActivity()).changeFragment(new Client_fragment());
+            }
+        });
+        Button button_modify_client = (Button) myView.findViewById(R.id.modify_client);
+
+        button_modify_client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Client client_modify = new Client ();
+
+
+                //client_modify.setId(id_client);
+                client_modify.setNom(value_nom.getText().toString());
+                client_modify.setPrenom(value_prenom.getText().toString());
+                client_modify.setPhone(Integer.valueOf(value_phone.getText().toString()));
+                client_modify.setNum_rue(Integer.valueOf(value_num_rue.getText().toString()));
+                client_modify.setNom_rue(value_nom_rue.getText().toString());
+                client_modify.setCp(Integer.valueOf(value_cp.getText().toString()));
+                client_modify.setVille(value_ville.getText().toString());
+
+                clientDAO.updateClient(client_modify);
+                MainActivity.closekeyboard(getContext(), myView);
+                ((MainActivity) getActivity()).changeFragment(new Client_fragment());
+            }
+        });
+
+        return myView;
+    }
+
+
+    private void showEditDialog() {
+
+    }
+
+
+
+}
